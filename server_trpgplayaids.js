59cb1ee41d800f7e2dd90e946acf893e8bf2daef
Yi <46272710+brushmen@users.noreply.github.com>
2019-09-03 23:10:36 +0800

    Update server_trpgplayaids.js

--- server_trpgplayaids.js
+++ server_trpgplayaids.js
@@ -5,9 +5,7 @@ var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
 
 var clientpages = {
-    'index': 'index.html',
     'archipelago': 'archipelago.html',
-    'bellsongs': 'bellsongs.html',
     'heartsofwulin': 'heartsofwulin.html',
     'omsl': 'omsl.html',
     'dicechat': 'dicechat.html'
@@ -20,7 +18,7 @@ var game_ui_path = 'game_uis';
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/' + clientpages['index']);
+    res.sendFile(__dirname + '/index.html');
 });
 
 // individual game HTML pages
@@ -28,9 +26,6 @@ app.get('/', function(req, res) {
 app.get('/archipelago', function(req, res) {
     res.sendFile(__dirname + '/' + game_ui_path + '/' + clientpages['archipelago']);
 });
-// app.get('/bellsongs', function(req, res) {
-//     res.sendFile(__dirname + '/' + game_ui_path + '/' + clientpages['bellsongs']);
-// });
 app.get('/heartsofwulin', function(req, res) {
     res.sendFile(__dirname + '/' + game_ui_path + '/' + clientpages['heartsofwulin']);
 });
@@ -45,23 +40,16 @@ app.get('/dicechat', function(req, res) {
 
 var helper = require('./helper.js');
 
-// game JS modules
-
-var archipelago = require('./game_modules/archipelago.js');
-// var bellsongs = require('./game_modules/bellsongs.js');
-var heartsofwulin = require('./game_modules/heartsofwulin.js');
-var omsl = require('./game_modules/omsl.js');
-var dicechat = require('./game_modules/dicechat.js');
+// main server code
 
 // start server
 
 server.listen(process.env.PORT || 3000);
 console.log('Server "tRPG Playaids" running...');
 
-// main server code
+// start game modules
 
-archipelago(app, io, helper);
-// bellsongs(app, io, helper);
-heartsofwulin(app, io, helper);
-omsl(app, io, helper);
-dicechat(app, io, helper);
+var archipelago = require('./game_modules/archipelago.js')(app, io, helper);
+var dicechat = require('./game_modules/dicechat.js')(app, io, helper);
+var heartsofwulin = require('./game_modules/heartsofwulin.js')(app, io, helper);
+var omsl = require('./game_modules/omsl.js')(app, io, helper);
