9737faee3bbfdd196f92f3f41a4cf845394d47e6
Yi <46272710+brushmen@users.noreply.github.com>
2019-09-03 23:12:23 +0800

    Update helper.js

--- helper.js
+++ helper.js
@@ -156,66 +156,6 @@ module.exports = {
         return arr.map(function (x, i) { return i });
     },
 
-    dragElement: function (elmnt) {
-        var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-        var element = document.getElementById(elmnt.id + "Header");
-        if (element) {
-            // if present, the header is where you move the DIV from:
-            element.onmousedown = dragMouseDown;
-        } else {
-            // otherwise, move the DIV from anywhere inside the DIV:
-            elmnt.onmousedown = dragMouseDown;
-        }
-
-        function dragMouseDown(e) {
-            e = e || window.event;
-            e.preventDefault();
-            // get the mouse cursor position at startup:
-            pos3 = e.clientX;
-            pos4 = e.clientY;
-            document.onmouseup = stopDragElement;
-            // call a function whenever the cursor moves:
-            document.onmousemove = elementDrag;
-        }
-
-        function elementDrag(e) {
-            e = e || window.event;
-            e.preventDefault();
-            // calculate the new cursor position:
-            pos1 = pos3 - e.clientX;
-            pos2 = pos4 - e.clientY;
-            pos3 = e.clientX;
-            pos4 = e.clientY;
-            // set the element's new position, but never far beyond broswer width or height
-            var newtop = elmnt.offsetTop - pos2;
-            var newleft = elmnt.offsetLeft - pos1;
-            var size = {
-                width: window.innerWidth || document.body.clientWidth,
-                height: window.innerHeight || document.body.clientHeight
-            }
-            var width = e.srcElement.clientWidth;
-            var height = e.srcElement.clientHeight;
-            if (newtop < 0) {
-                newtop = 0;
-            } else if ((newtop + height) > size.height) {
-                newtop = size.height - height;
-            }
-            if (newleft < 0) {
-                newleft = 0;
-            } else if ((newleft + width) > size.width) {
-                newleft = size.width - width;
-            }
-            elmnt.style.top = newtop + "px";
-            elmnt.style.left = newleft + "px";
-        }
-
-        function stopDragElement() {
-            // stop moving when mouse button is released:
-            document.onmouseup = null;
-            document.onmousemove = null;
-        }
-    },
-
     diceRoller: function (str) {
         var output = str;
         var matches;
